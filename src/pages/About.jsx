1bc952ad73f0d6dd2951596f37f02c9ca5548019
~/alpha <s201830040@kfupm.edu.sa>
2022-03-29 16:58:32 +0300

    update index & about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Crypto = () => {
     return (
         <div className='bg-gradient-to-r from-sky-500 to-indigo-500 h-screen'>
             <nav className='flex flex-row justify-around w-10/12 m-auto pt-10 font-thin text-xl text-white  font-raleway uppercase'>
-                <Link to="/">Home</Link>
-                <Link to="/hash">Hash</Link>
-                <Link to="/crypto">Crypto</Link>
-                <Link to="/about">About</Link>
+                <Link to='/'>Home</Link>
+                <Link to='/hash'>Hash</Link>
+                <Link to='/crypto'>Crypto</Link>
+                <Link to='/about'>About</Link>
+            </nav>
+            <div className='w-10/12 m-auto flex flex-col justify-center items-center mt-60'>
+                <h1 className='text-8xl text-white font-raleway'>~/CrytpoX</h1>
+                <p className='text-xl mt-20 text-white font-raleway font-thin text-center w-4/6'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias laboriosam, ducimus inventore cum mollitia modi. Esse, corporis repudiandae illum sed ducimus iure eum laborum quis. Autem, minima blanditiis ratione cupiditate dolor alias officia nobis non nihil nemo sint labore totam expedita, itaque repellat? Labore illum aliquid explicabo, similique.s
+                </p>
+            </div>
+
+            <nav className='flex flex-row justify-evenly w-10/12 m-auto pt-40 font-thin text-xl text-white font-raleway uppercase'>
+                <a href="www.twitter.com">Twitter</a>
+                <a href="www.linkedin">LinkedIn</a>
+                <a href="www.github.com">Github</a>
+                <a href="www.github.com">Contact Us</a>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Crypto
+export default Crypto;
